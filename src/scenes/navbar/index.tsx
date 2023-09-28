b776aa484c2992f42e1394da1528fe6b4c93354c
platisam <platisam@gmail.com>
2023-09-28 16:00:07 +0200

    building Navbar component

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -6,7 +6,33 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   const flexBetween = "flex items-center justify-between";
-  return <div></div>;
+
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img src={Logo} alt="logo" />
+
+            {/* RIGHT SIDE */}
+            <div className={`${flexBetween} w-full `}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benefits</p>
+                <p>Our Classes</p>
+                <p>Contact Us</p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
