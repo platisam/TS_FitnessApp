7daf7353ce280de5bda64a3aa64282a78c79bb3f
platisam <platisam@gmail.com>
2023-09-30 17:27:54 +0200

    adding sponsors and styling home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,6 +1,10 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "../../shared/types";
 import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
 import ActionButton from "@/shared/ActionButton";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
@@ -14,17 +18,17 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* MAIN HEADER */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the BodyShapes That you Dream of... Get Your Dream
               Body Now.
@@ -32,7 +36,7 @@ const Home = ({ setSelectedPage }: Props) => {
           </div>
 
           {/* ACTIONS */}
-          <div>
+          <div className="mt-8 flex items-center gap-8">
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -47,8 +51,23 @@ const Home = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* IMAGE */}
-        <div></div>
+        <div>
+          <img src={HomePageGraphic} alt="home-page-graphic" />
+        </div>
       </div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div>
+          <div>
+            <div>
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorForbes} alt="forbes-sponsor" />
+              <img src={SponsorFortune} alt="fortune-sponsor" />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
