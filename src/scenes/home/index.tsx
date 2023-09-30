e598aae3d3fbab1dce712f898c4975a756e48b82
platisam <platisam@gmail.com>
2023-09-30 16:47:48 +0200

    building Home page using heading and actions

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,5 +1,8 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "../../shared/types";
+import HomePageText from "@/assets/HomePageText.png";
+import ActionButton from "@/shared/ActionButton";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -8,7 +11,46 @@ type Props = {
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
-  return <div>Home</div>;
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <div>
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p>
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the BodyShapes That you Dream of... Get Your Dream
+              Body Now.
+            </p>
+          </div>
+
+          {/* ACTIONS */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+
+        {/* IMAGE */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
