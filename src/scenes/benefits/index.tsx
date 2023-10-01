fe80afe7d8301b15596617a5d6352c14c731f6da
platisam <platisam@gmail.com>
2023-10-01 14:33:47 +0200

    creating benefits array of objects

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -7,6 +7,27 @@ import {
 } from "@heroicons/react/24/solid";
 import HText from "@/shared/HText";
 
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, mollitia? Sequi repudiandae veniam odit iure.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, mollitia? Sequi repudiandae veniam odit iure.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, mollitia? Sequi repudiandae veniam odit iure.",
+  },
+];
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -17,8 +38,21 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div>
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
           <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </div>
+
+        {/* BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+          {benefits.map((benefit) => (
+            <Benefit />
+          ))}
         </div>
       </motion.div>
     </section>
