6c8023a23ba1a3640acfb096e264d45c94b50dde
platisam <platisam@gmail.com>
2023-10-02 11:58:10 +0200

    creating and adding OurClasses component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types.ts";
 import Home from "./scenes/home";
 import Benefits from "./scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -31,6 +32,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,48 @@
+import { SelectedPage } from "@/shared/types";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import image5 from "../../assets/image5.png";
+import image6 from "../../assets/image6.png";
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              rem eligendi assumenda quasi cumque magni architecto totam
+              provident repellat vel. Consequatur quis, ut, suscipit molestias
+              rerum qui voluptatum debitis sit sed perferendis laudantium culpa
+              iusto autem quibusdam ad deleniti deserunt?
+            </p>
+          </div>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurClasses;
