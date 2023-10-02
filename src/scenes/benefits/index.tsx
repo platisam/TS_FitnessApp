db24ed05186fb315b17b4817dcbc85dbb994f749
platisam <platisam@gmail.com>
2023-10-02 11:21:03 +0200

    setting animation and completing benefits page

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -8,6 +8,7 @@ import {
 import HText from "@/shared/HText";
 import Benefit from "./Benefit";
 import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "../../assets/BenefitsPageGraphic.png";
 
 const benefits: Array<BenefitType> = [
   {
@@ -86,26 +87,48 @@ const Benefits = ({ setSelectedPage }: Props) => {
           ))}
         </motion.div>
         {/* GRAPHICS AND DESCRIPTION */}
-        <div>
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
           {/* GRAPHIC */}
-          <img src="" alt="" />
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefits-page-graphic"
+          />
 
           {/* DESCRIPTION */}
           <div>
             {/* TITLE */}
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
-                <div>
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
                   <HText>
                     MILLIONS OF HAPPY MEMBERS GETTING{" "}
                     <span className="text-primary-500">FIT</span>
                   </HText>
-                </div>
+                </motion.div>
               </div>
             </div>
 
             {/* DESCRIPTION */}
-            <div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <p className="my-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
                 itaque vitae neque nostrum a aspernatur at, incidunt aperiam
@@ -121,7 +144,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
                 sunt consequatur necessitatibus omnis quod debitis iste. Natus
                 fuga magni vitae, adipisci quas aut rem laudantium ipsam.
               </p>
-            </div>
+            </motion.div>
 
             {/* BUTTON */}
             <div className="relative mt-16">
