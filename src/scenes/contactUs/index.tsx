30007437c6a9145b8d533927df11c9fd90e0d5fc
platisam <platisam@gmail.com>
2023-10-03 11:03:51 +0200

    building header section of the ContactUs page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,7 +1,45 @@
-type Props = {};
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "../../assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
 
-const ContactUs = (props: Props) => {
-  return <div>Contact Us</div>;
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore unde
+            veniam eligendi nam culpa odit sed temporibus quas distinctio,
+            molestiae itaque harum quae cupiditate magnam et voluptas? Placeat,
+            vitae itaque.
+          </p>
+        </motion.div>
+        {/* FORM AND IMAGE */}
+      </motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
