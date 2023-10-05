1e057a4670c2a9f40f574051cfd1141412b4ce7a
platisam <platisam@gmail.com>
2023-10-05 16:39:59 +0200

    setting first layout to footer section

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,7 +1,24 @@
-import Logo from "@/assets/Logo.png";
+import Logo from "../../assets/Logo.png";
 
 const Footer = () => {
-  return <footer className="bg-primary-100 py-16">Footer</footer>;
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur tempora impedit asperiores velit similique. Doloremque
+            maxime consequuntur, totam nisi eveniet modi, obcaecati ad pariatur,
+            ducimus omnis nam quam reiciendis vitae.
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
