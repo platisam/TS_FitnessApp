60014d7de12e91dc0c964fb459ce35df96104df7
platisam <platisam@gmail.com>
2023-10-05 16:53:56 +0200

    finishing the footer of the page

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -14,8 +14,17 @@ const Footer = () => {
           </p>
           <p>© Evogym All Rights Reserved.</p>
         </div>
-        <div></div>
-        <div></div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>(333)425-6825</p>
+        </div>
       </div>
     </footer>
   );
